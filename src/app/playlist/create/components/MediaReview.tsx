58bf6d47238b898d290063f3944165de278921d1
Ben <rhuang10@mail.sfsu.edu>
2024-03-17 11:53:24 -0700

    Add final tweaks to create page, add howto section to home page

--- src/app/playlist/create/components/MediaReview.tsx
+++ src/app/playlist/create/components/MediaReview.tsx
@@ -32,18 +32,18 @@ export default function MediaReview(props: MediaReviewProps) {
 
   return (
     <div className={`container mt-5 ${props.currentPage <= 2 ? "d-none" : ""}`}>
-      <div className="d-flex justify-content-center">
+      <div className="row justify-content-center">
         {props.selectedTitles.map((title,i) => (
-          <div className={`${styles["breakpoint-width"]} pb-5`}  key={title.id}>
+          <div className={`${styles["breakpoint-width"]} pb-5 ${containerClassName} `}  key={title.id}>
           <div  className={`d-flex mt-5 ${styles["review-container"]} justify-content-sm-between justify-content-start ${containerClassName}`}>
-            <div className={`${styles["poster-container"]} col-lg-3 col text-center`}>
+            <div className={`${styles["poster-container"]} col-xl-3 col-lg-4 col text-center`}>
               <Image className={`rounded ${styles["box-shadow"]}`} alt="movie pic" width="280" height="400" src={`https://image.tmdb.org/t/p/original/${title.mediaPoster}`}></Image>
-              <div className="d-flex justify-content-center text-primary">
-                <span className={`${classNames("text-center mt-4", { [styles["multiline-ellipsis-tagline"]]: title.tagline }, { "text-danger": !title.tagline })} `}>{title.tagline || "Tagline Unavailable"}</span>
-                <span className={`${classNames({ [styles["end-quote"]]: title.tagline })}`}></span>
+              <div className="d-flex flex-column text-primary">
+                <span className={`${classNames("text-center mt-4", { "text-danger": !title.tagline })} `}>{title.tagline || "Tagline Unavailable"}</span>
+                <IoIosArrowDown key={title.id} onClick={(e:React.MouseEvent) => toggleReviewDropdown(e, title.id)} className={`${classNames(styles["dropdown-review-arrow"], { [styles["rotate"]]: isReviewOpen[title.id] })} d-block d-md-none mx-auto`} color="white" size="3.5em"></IoIosArrowDown>
               </div>
             </div>
-            <div className={`p-5 ms-3 position-relative col-lg-9 col-7  d-md-block d-none`}>
+            <div className={`p-5 ms-3 position-relative col-xl-9 col-lg-8 col-7 d-md-block d-none`}>
             <h4  className={`${styles["numbered-circle"]}`}>{i + 1}</h4>
               <h2>
                 <a className={`${styles["imbd-link"]}`} target="_blank" href={`https://www.imdb.com/title/${title.imdb}/`}>
@@ -72,26 +72,25 @@ export default function MediaReview(props: MediaReviewProps) {
                   {title.trailer == undefined ? "Trailer Not Found" : "View Trailer"}
                 </a>
               </button>
-            
               <IoIosArrowDown key={title.id} onClick={(e:React.MouseEvent) => toggleReviewDropdown(e, title.id)} className={`${classNames(styles["dropdown-review-arrow"], { [styles["rotate"]]: isReviewOpen[title.id] })}`} color="white" size="3.5em"></IoIosArrowDown>
             </div>
           </div>
           <div className={`${styles["review-container-accordion"]} ${containerClassName} ${styles["accordion-content"]} ${classNames({ [styles["active"]]: isReviewOpen[title.id] })}`}>
-            <div className="d-flex flex-column  p-3">
+            <div className="d-flex flex-column p-3">
             <hr className="mx-4"></hr>
             <div className="text-center align-self-center">Write a review · {title.mediaName} ({title.mediaYear})</div>
               <div className="d-flex justify-content-between">
               <div className={`${styles["rate"]}`}>
-              {[5,4,3,2,1].map((value)=> (
-               <>
-                  <input type="radio" id={`star${value}-${title.id}`} name={`${title.id}`} value={value} />
+              {[5,4,3,2,1].map((value,i)=> (
+  <React.Fragment key={i}>
+  <input type="radio" id={`star${value}-${title.id}`} name={`${title.id}`} value={value} />
                   <label htmlFor={`star${value}-${title.id}`} title={`${value} Star`}>
                     {value} stars
                   </label>
-                </>
+                </React.Fragment>
               ))}
             </div>
-              <div className="align-self-center">Review by User: <span className="text-success">Ben</span></div>
+              <div className="align-self-center"><span className="text-success">Ben</span></div>
               </div>
             <textarea placeholder="Your feedback helps others decide which movies to watch." className={`mt-3 form-control light-border-input ph-color-white`} maxLength={3000} rows={9}></textarea>
             </div>
